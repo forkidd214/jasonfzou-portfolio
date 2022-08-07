df618480adfd52adcdde2c0af34e742f7d0d2f9b
forkidd214 <forkidd214@gmail.com>
2022-08-08 00:00:38 +0800

    Update lint-staged and huky config. [Todo] to fix a bug @'./.husky/pre-commit'

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -6,19 +6,8 @@ const buildEslintCommand = (filenames) =>
     .join(' --file ')}`
 
 module.exports = {
-  // '**/*.{js,jsx,ts,tsx}': [buildEslintCommand],
-  '*.{js,jsx,ts,tsx}': ['eslint --fix', 'git add'],
-
-  // // this will check Typescript files
-  // '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
-
-  // // This will lint and format TypeScript and                                             //JavaScript files
-  // '**/*.(ts|tsx|js)': (filenames) => [
-  //   `yarn eslint --fix ${filenames.join(' ')}`,
-  //   `yarn prettier --write ${filenames.join(' ')}`,
-  // ],
-
-  // // this will Format MarkDown and JSON
-  // '**/*.(md|json)': (filenames) =>
-  //   `yarn prettier --write ${filenames.join(' ')}`,
+  '**/*.{ts,tsx}': () => 'yarn tsc --noEmit',
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand], //nextjs suggestion
+  // '*.{js,jsx,ts,tsx}': 'eslint --fix',
+  '*.{md,html,css,json}': 'prettier --write',
 }
